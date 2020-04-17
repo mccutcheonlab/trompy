c4f336331c177f35afe0637f71198573ef0b5177
James Edgar McCutcheon <jmc010@uit.no>
2020-04-17 15:54:41 +0200

    Fixed errors

--- trompy/__init__.py
+++ trompy/__init__.py
@@ -2,5 +2,5 @@ from trompy.barscatter import barscatter, setcolors, data2obj1D, data2obj2D, xys
 from trompy.medfilereader import medfilereader, isnumeric
 from trompy.metafile_utils import metafilemaker, metafilereader
 from trompy.snipper_utils import snipper, mastersnipper, zscore, findnoise, removenoise, med_abs_dev, makerandomevents, time2samples, event2sample
-from general_utils import remcheck, random_array
-from lick_utils import lickCalc, removeshortbursts, calculate_burst_prob, weib_davis, fit_weibull
+from trompy.general_utils import remcheck, random_array
+from trompy.lick_utils import lickCalc, removeshortbursts, calculate_burst_prob, weib_davis, fit_weibull
