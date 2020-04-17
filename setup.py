c22aa6c547fd227c5f2e21aa006183ad93402c20
James Edgar McCutcheon <jmc010@uit.no>
2020-04-17 11:34:31 +0200

    removed csv from required packages

--- setup.py
+++ setup.py
@@ -18,8 +18,7 @@ setup(
   install_requires=[            # I get to this in a second
           'numpy',
           'matplotlib',
-          'xlrd',
-          'csv'       
+          'xlrd'       
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
