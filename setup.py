b1463c50fd475c5d47778559df833e33ced6cd66
James Edgar McCutcheon <jmc010@uit.no>
2020-04-17 16:31:15 +0200

    removed colorsys from required imports

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ setup(
           'numpy',
           'matplotlib',
           'xlrd',
-          'scipy',
-          'colorsys'
+          'scipy'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
