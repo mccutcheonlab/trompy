86c629e61ce915f8ec5300e0b35c873a17d7cf99
James Edgar McCutcheon <jmc010@uit.no>
2020-04-18 21:01:39 +0200

    Update __init__.py

--- trompy/__init__.py
+++ trompy/__init__.py
@@ -6,5 +6,6 @@ from trompy.general_utils import remcheck, random_array, getuserhome, flatten_li
 from trompy.lick_utils import lickCalc, removeshortbursts, calculate_burst_prob, weib_davis, fit_weibull
 from trompy.stats_utils import sidakcorr, mean_and_sem, bonferroni_corrected_ttest
 from trompy.fig_utils import setsameaxislimits, invisible_axes, shadedError, ax2prop, lighten_color, get_violinstats
-from trompy.lick_figs import licklengthFig, iliFig, burstlengthFig, ibiFig
+from trompy.lick_figs import licklengthFig, iliFig, burstlengthFig, ibiFig, burstprobFig, sessionlicksFig
 from trompy.trials_figs import trialsFig, trialsMultFig, trialsShadedFig, trialsMultShadedFig, trialstiledFig
+from trompy.lick_gui import Window, get_location, alert, checknsessions, medfilereader_licks, tstamp_to_tdate, start_gui
