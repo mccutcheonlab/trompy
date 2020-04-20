87df01372fca6cf4ed48f50934df4fed01f519ae
James Edgar McCutcheon <jmc010@uit.no>
2020-04-20 15:18:16 +0200

    fixed get_location

--- trompy/gui_utils.py
+++ trompy/gui_utils.py
@@ -6,9 +6,11 @@ Created on Mon Apr 20 13:03:34 2020
 """
 from tkinter import filedialog
 from tkinter import messagebox
+import os
 
 def get_location():
-    loc = filedialog.askdirectory(initialdir=currdir, title='Select a save folder.')
+    loc = filedialog.askdirectory(initialdir=os.getcwd(), title='Select a save folder.')
+    os.chdir(loc)
     return loc
        
 def alert(msg):
