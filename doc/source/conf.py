2c5ac323dd0c53a4d2372aeb74d734dbbf8e8032
Jaime McCutcheon <mccutcheon.james@gmail.com>
2020-04-24 12:09:53 +0200

    Prep for 0.9.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, James E McCutcheon'
 author = 'James E McCutcheon'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.3-alpha'
+release = '0.9.0-alpha'
 
 
 # -- General configuration ---------------------------------------------------
