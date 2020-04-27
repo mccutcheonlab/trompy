289e0b62c3d43d2397a323e6a82547e34d68e172
James Edgar McCutcheon <jmc010@uit.no>
2020-04-27 22:09:56 +0200

    Prep for 0.9.2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, James E McCutcheon'
 author = 'James E McCutcheon'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.1-alpha'
+release = '0.9.2-alpha'
 
 
 # -- General configuration ---------------------------------------------------
