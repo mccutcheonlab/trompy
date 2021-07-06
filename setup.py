ad376ca20b2258efc517a1455e63a519f600cf75
Jaime McCutcheon <mccutcheon.james@gmail.com>
2021-07-06 15:11:09 +0200

    Prep for v0.13.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup
 setup(
   name = 'trompy',         # How you named your package folder (MyLib)
   packages = ['trompy'],   # Chose the same as "name"
-  version = '0.13.2',      # Start with a small number and increase it with every change you make
+  version = '0.13.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A collection of tools for data analysis and plotting that originated in the McCutcheon Lab (UiT, Tromso)',   # Give a short description about your library
   long_description = long_description,
@@ -22,7 +22,7 @@ setup(
   author = 'James E McCutcheon',                   # Type in your name
   author_email = 'j.mccutcheon@uit.no',      # Type in your E-Mail
   url = 'https://github.com/mccutcheonlab',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mccutcheonlab/trompy/archive/v0.13.2-beta.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/mccutcheonlab/trompy/archive/v0.13.3-beta.tar.gz',    # I explain this later on
   keywords = ['neuroscience', 'behavior'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
