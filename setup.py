7f8dbc35563b7179f8ef9132308c0ee8ebe47f12
James Edgar McCutcheon <jmc010@uit.no>
2022-03-20 14:16:35 +0100

    Prep for 0.14.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup
 setup(
   name = 'trompy',         # How you named your package folder (MyLib)
   packages = ['trompy'],   # Chose the same as "name"
-  version = '0.13.7',      # Start with a small number and increase it with every change you make
+  version = '0.14.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A collection of tools for data analysis and plotting that originated in the McCutcheon Lab (UiT, Tromso)',   # Give a short description about your library
   long_description = long_description,
@@ -22,7 +22,7 @@ setup(
   author = 'James E McCutcheon',                   # Type in your name
   author_email = 'j.mccutcheon@uit.no',      # Type in your E-Mail
   url = 'https://github.com/mccutcheonlab',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mccutcheonlab/trompy/archive/v0.13.7-beta.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/mccutcheonlab/trompy/archive/v0.14.0-beta.tar.gz',    # I explain this later on
   keywords = ['neuroscience', 'behavior'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
@@ -33,7 +33,7 @@ setup(
           'tdt'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
@@ -41,5 +41,8 @@ setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
   ],
 )
