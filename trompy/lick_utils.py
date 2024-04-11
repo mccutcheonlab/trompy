f16740a2d2219f06ba8e905faceee013be458a24
jaimemcc <j.mccutcheon@uit.no>
2024-04-11 10:36:10 +0200

    Fixed bug affecting run calculation in Lickcalc

--- trompy/lick_utils.py
+++ trompy/lick_utils.py
@@ -145,11 +145,13 @@ def lickCalc(licks, offset = [], burstThreshold = 0.5, runThreshold = 10,
 if __name__ == '__main__':
     print('Testing functions')
     import trompy as tp
-    filename = Path("C:/Users/jmc010/Github/trompy/tests/test_data/03_W.med")
+    filename = Path("C:/Github/trompy/tests/test_data/03_W.med")
 
     data = tp.medfilereader(filename, vars_to_extract=["e", "f"], remove_var_header=True)
 
-    lickdata = lickCalc(data[0], offset=data[1], minburstlength=10)
+    lickdata = lickCalc(data[0], offset=data[1], minburstlength=1)
+    
+    print(lickdata["rEnd"])
 
     # print(lickdata['freq'])
 
